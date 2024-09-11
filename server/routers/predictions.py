cad3cbbef3dc201cc5502ef0426ac29521ce3123
RichardNooooh <richard.jun.noh@gmail.com>
2024-09-11 10:04:46 -0500

    Add /predictions/ data structures

--- server/routers/predictions.py
+++ server/routers/predictions.py
@@ -1,18 +1,30 @@
-from fastapi import APIRouter()
-from pydantic import BaseModel
+# Method 4 from https://stackoverflow.com/a/70640522/19918922
+from fastapi import AppRouter, File, Body, UploadFile
+from pydantic import BaseModel, model_validator
+from typing import Optional
 from uuid import UUID
+import json
+
+class PredictionRequest(BaseModel):
+    model_id: UUID
+
+    @model_validator(mode='before')
+    @classmethod
+    def validate_to_json(cls, value):
+        if isinstance(value, str):
+            return cls(**json.loads(value))
+        return value
+
+class PredictionResult(BaseModel):
+    classification: int
+    probability: float
+
+class PredictionResponse(BaseModel):
+    model_id: UUID
+    results: list[PredictionResponse]
 
 router = APIRouter()
 
-class BasePrediction(BaseModel):
-    model_id: UUID
-
-class Prediction_In(BasePrediction):
-    input_data: 
-
-
-@router.post("/predictions/", tags=["predictions"])
-async def load_prediction() -> list[str]:
-    if not model_name:
-        # SQL: SELECT name FROM datasets
-        return None # read list of names
+@router.post("/predictions/")
+async def load_predictions(data: PredictionRequest = Body(...), files: list[UploadFile] = File(...)) -> PredictionResponse:
+    return {"JSON Payload": data, "Filenames": [file.filename for file in files]}
