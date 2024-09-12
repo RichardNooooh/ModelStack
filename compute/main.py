f784a332c6ef24628484646f7ca03a1c0e056587
RichardNooooh <richard.jun.noh@gmail.com>
2024-09-12 07:24:29 -0500

    Fix job control POST request

--- compute/main.py
+++ compute/main.py
@@ -25,9 +25,7 @@ class JobControlHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self._set_headers()
         JobQueue.lock.acquire()
-        # try:
         q = [json.dumps(job, cls=JobEncoder) for job in JobQueue.queue]
-        # finally:
         JobQueue.lock.release()
 
         self.wfile.write(json.dumps({"queue": q}).encode('utf-8'))
@@ -35,21 +33,20 @@ class JobControlHandler(BaseHTTPRequestHandler):
     def do_POST(self):
         print("> Entered POST")
         length = int(self.headers.get('content-length'))
-        message = json.loads(self.rfile.read(length))
+        body_data = self.rfile.read(length).decode('utf-8')
+        message = json.loads(body_data)
         
         # add a property to the object, just to mess with data
         message['received'] = 'ok'
-        print("> About to acquire lock")
         JobQueue.lock.acquire()
-        print("> Acquired lock")
-        try:
-            JobQueue.queue.append(Job(uuid.uuid4()))
-        finally:
-            JobQueue.lock.release()
-        print("> Released lock")
+        JobQueue.queue.append(Job(uuid.uuid4()))
+        JobQueue.lock.release()
         # send the message back
         self._set_headers()
-        self.wfile.write(json.dumps(message).encode('utf-8'))
+
+        return_message = json.dumps(message).encode('utf-8')
+        print(return_message)
+        self.wfile.write(return_message)
     
 
 def run_http_server():
@@ -62,9 +59,7 @@ if __name__ == "__main__":
     # we run the job q system indefinitely..
     while True:
         JobQueue.lock.acquire()
-        try:
-            print(len(JobQueue.queue))
-        finally:
-            JobQueue.lock.release()
+        print(len(JobQueue.queue))
+        JobQueue.lock.release()
 
         sleep(3.0)
