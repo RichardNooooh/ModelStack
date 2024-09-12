b1048d1d511de21cad083b5ca5acce8f950e8da3
RichardNooooh <richard.jun.noh@gmail.com>
2024-09-12 08:12:25 -0500

    Reset frontend code to template

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+// import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
+
+const authConfig = {
+    domain: import.meta.env.VITE_AUTH0_DOMAIN,
+    clientId: import.meta.env.VITE_AUTH0_CLIENTID,
+    authorizationParams: {
+        redirect_uri: window.location.origin,
+    },
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Auth0Provider {...authConfig}>
+      <App />
+    </Auth0Provider>
   </StrictMode>,
 )
